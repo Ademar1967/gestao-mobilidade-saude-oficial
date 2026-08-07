79175d34524fd054f3b9f5734dfe174e33594d36
Ademar1967 <ademarp2015@gmail.com>
2026-08-07 13:16:04 -0300

    fix: atualiza pip no build e remove django-admin-interface/extensions (suspeitos Rust)

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-﻿pip install --no-cache-dir --prefer-binary -r requirements-prod.txt
+﻿pip install --upgrade pip
+pip install --no-cache-dir --prefer-binary -r requirements-prod.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
